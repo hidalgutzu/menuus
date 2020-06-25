cd22bd2de0edd293b780952dba0f05c0077805bd
Daniel Hidalgo <hidalgutzu@gmail.com>
2020-06-25 16:37:13 -0500

    Todo el Sitio
    se subió todo el contenido de menuus.app

---
+++ assets/js/javascript.js
@@ -0,0 +1,32 @@
+// ---------- MENÚ HAMBURGUESA Y FONDO ESTÁTICO ----------
+// llamamos a body para poder modificarlo
+const body = document.querySelector('.body');
+// llamamos al menú
+const menu = document.querySelector('.burger-menu');
+// llamamos al menú principal
+const menuPrincipal = document.querySelector('.menu-principal');
+// llamamos al botón "hamburguesa"
+const burgerButton = document.querySelector('.burger-button');
+// llamamos al botón "cerrar"
+const closeButton = document.querySelector('#close-menu');
+
+// CLICK para activar las funciones  
+burgerButton.addEventListener('click', show)
+closeButton.addEventListener('click', hide)
+
+// la función ADD
+function show() {
+menu.classList.remove('hidebm');
+closeButton.classList.remove('ocultar');
+menuPrincipal.classList.add('ocultar');
+menu.classList.add('is-active');
+body.classList.add('hideoverflow');
+}
+// la función REMOVE
+function hide() {
+menu.classList.add('hidebm');
+closeButton.classList.add('ocultar');
+menuPrincipal.classList.remove('ocultar');
+menu.classList.remove('is-active');
+body.classList.remove('hideoverflow');
+}
