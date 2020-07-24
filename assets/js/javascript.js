7198b10cd5c056db2d849fd067b74d581137db9e
Daniel Hidalgo <hidalgutzu@gmail.com>
2020-07-24 17:44:57 -0500

    botón-play en home

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -30,3 +30,36 @@ menuPrincipal.classList.remove('ocultar');
 menu.classList.remove('is-active');
 body.classList.remove('hideoverflow');
 }
+
+
+/////////////
+
+
+       // 2. This code loads the IFrame Player API code asynchronously.
+       var tag = document.createElement('script');
+
+       tag.src = "https://www.youtube.com/iframe_api";
+       var firstScriptTag = document.getElementsByTagName('script')[0];
+       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+ 
+       // 3. This function creates an <iframe> (and YouTube player)
+       //    after the API code downloads.
+       var player;
+       function onYouTubeIframeAPIReady() {
+         player = new YT.Player('player', {
+           height: '390',
+           width: '640',
+           videoId: YOUR_ID_HERE,
+           playerVars: {
+             controls: 0,
+             showinfo:0,
+             rel:0,
+           },
+           events: {
+             'onReady': onPlayerReady,
+           }
+         });
+       }
+ 
+
+//Youtube Modal script
